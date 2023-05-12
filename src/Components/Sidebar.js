b870487515e8823cb98f4a1ada9cbc4fdb66c57f
ManausK <1905246@kiit.ac.in>
2023-05-12 20:24:17 +0530

    Final css and data done

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -11,12 +11,13 @@ function Sidebar() {
             <li
               key={key}
               className="row"
+              id={window.location.pathname == val.link ? "active" : ""}
               onClick={() => {
                 window.location.pathname = val.link;
               }}
             >
               {" "}
-              <div>{val.icon}</div> <div>{val.title}</div>
+              <div id="icon">{val.icon}</div> <div id="title">{val.title}</div>
             </li>
           );
         })}
